ebb871165357c643e9ff56e17a9c4ac92c2531b9
bleedingpoem <kamran.hossain22@yahoo.com>
2017-10-22 06:59:35 +0600

    Rest and peace with Django Rest Framework Set Up

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,9 +46,12 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = (
+    'rest_framework',
 )
 
 LOCAL_APPS = (
+    'project.api',
+
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -147,3 +150,6 @@ STATICFILES_FINDERS = (
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = str(APPS_DIR('media'))
+
+REST_FRAMEWORK = {
+}
